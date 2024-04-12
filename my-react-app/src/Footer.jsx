3cd9d96557f1deff4c9aa4217597bb352cc96223
pat taylor <patrick.schneider9@web.de>
2024-04-12 12:22:37 +0200

    feat: add header and footer

---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer(){
+    return(
+        <>
+            <footer>
+                <p>&copy; {new Date().getFullYear()} The footer</p>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+    return(
+        <header>
+            <h1>My Website for you!</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <hr></hr>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
