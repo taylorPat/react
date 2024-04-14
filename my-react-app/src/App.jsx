fb737150ba7a59d382f10699d1ab403bcd7285ae
pat taylor <patrick.schneider9@web.de>
2024-04-14 15:57:15 +0200

    feat(darts): refactor darts score board without using document.getElementById

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
+import UseStateComp from './UseStateComp.jsx'
 import CarList from './CarList.jsx'
 
 function App() {
   return( <>
             <Header/>
+            <UseStateComp />
             <CarList />
             <Footer/>
           </>)
--- my-react-app/src/UseStateComp.jsx
+++ my-react-app/src/UseStateComp.jsx
@@ -1,29 +1,49 @@
 import { useState } from "react"
 
 function UseStateComp() {
-    const [scorePlayer1, setScorePlayer1] = useState(101);
-    const [scorePlayer2, setScorePlayer2] = useState(501);
+    const startingPoints = 101
+    const [pointsP1, setPointsP1] = useState(startingPoints);
+    const [scoreP1, setScoreP1] = useState("");
 
-    const addScore = (score, currentScore, setScore) => {
-        let newScore = currentScore - score;
+    
+    const [pointsP2, setPointsP2] = useState(startingPoints);
+    const [scoreP2, setScoreP2] = useState("");
+
+    function changePointsP1(e) {
+        setScoreP1(e.target.value)
+    }
+
+    function changePointsP2(e) {
+        setScoreP2(e.target.value)
+    }
+
+    const addScore = (score, currentPoints, setPoints, setScore) => {
+        let newScore = currentPoints - score;
         if (newScore === 0) {
             newScore = 501
         }
         if (newScore <= 0) {
-            newScore = currentScore 
+            newScore = currentPoints 
+            window.alert("Watch out!")
         }
-        setScore(newScore)
+        setPoints(newScore)
+        setScore("");
     }
 
     return(
-        <div>
-            <p>Player 1: {scorePlayer1}</p>
-            <input type="number" id="inputP1"></input>
-            <button onClick={() => addScore(document.getElementById("inputP1").value, scorePlayer1, setScorePlayer1)}>Add score</button>
-            <p>Player 2: {scorePlayer2}</p>
-            <input type="number" id="inputP2"></input>
-            <button onClick={() => addScore(document.getElementById("inputP2").value, scorePlayer2, setScorePlayer2)}>Add score</button>
-        
+        <div className="Score-board">
+            <div className="Score-player">
+                <p>Player 1</p>
+                <p>{pointsP1}</p>
+                <input type="number" value={scoreP1} placeholder="Enter points" onChange={changePointsP1}></input>
+                <button onClick={() => addScore(scoreP1, pointsP1, setPointsP1, setScoreP1)}>Add score</button>
+            </div>
+            <div className="Score-player">
+                <p>Player 2</p>
+                <p>{pointsP2}</p>
+                <input type="number" value={scoreP2} placeholder="Enter points" onChange={changePointsP2}></input>
+                <button onClick={() => addScore(scoreP2, pointsP2, setPointsP2, setScoreP2)}>Add score</button>
+            </div>
         </div>
     )
 }
