78c9ac5b7a0f5569a42059da751440c42175e1bc
pat taylor <patrick.schneider9@web.de>
2024-04-15 23:07:19 +0200

    feat(todo): rest of todo list

--- my-react-app/src/TodoList.jsx
+++ my-react-app/src/TodoList.jsx
@@ -2,9 +2,33 @@ import { useState } from "react";
 
 function TodoList(){
     const [todo, setTodo] = useState("");
+    const [todoList, setTodoList] = useState([]);
 
     function handleTodoChange(event){
         setTodo(event.target.value)
+    }
+
+    function addTodo(){
+        setTodoList(t => [...t, todo]);
+        setTodo("");
+    }
+
+    function deleteTodo(index) {
+        setTodoList(t => t.filter((_, i) => i !== index))
+    }
+
+    function moveUp(index) {
+        if (index > 0) {
+            [todoList[index-1], todoList[index]] = [todoList[index], todoList[index-1]];
+            setTodoList(() => [...todoList])
+        }
+    }
+
+    function moveDown(index) {
+        if (index < todoList.length - 1) {
+            [todoList[index+1], todoList[index]] = [todoList[index], todoList[index+1]];
+            setTodoList(() => [...todoList])
+        }
     }
 
     return(
@@ -13,6 +37,19 @@ function TodoList(){
                 value={todo} 
                 placeholder="Enter your todo..."
                 onChange={handleTodoChange} />
+            <button onClick={addTodo}>Enter</button>
+
+            <div>
+                <ol>
+                    {todoList.map((todo, index) => (
+                            <li className="todo-item" key={index}>{todo}
+                            <button onClick={() => deleteTodo(index)}>Delete</button>
+                            <button onClick={() => moveUp(index)}>Up</button>
+                            <button onClick={() => moveDown(index)}>Down</button></li>
+                            )
+                        )}
+                </ol>
+            </div>
         </div>
     )
 }
